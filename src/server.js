72e1aafb4fc8a95485fd996c358bfeca3ebafbca
Dusty850 <105994589+Dusty850@users.noreply.github.com>
2026-05-06 16:05:04 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -101,28 +101,17 @@ app.get('/linked-role', async (req, res) => {
  * metadata endpoint. 
  */
 async function updateMetadata(userId) {
-  // Fetch the Discord tokens from storage
   const tokens = await storage.getDiscordTokens(userId);
-    
   let metadata = {};
   try {
-    // Fetch the new metadata you want to use from an external source. 
-    // This data could be POST-ed to this endpoint, but every service
-    // is going to be different.  To keep the example simple, we'll
-    // just generate some random data. 
+    // We are manually setting this to 1 (True) 
+    // so anyone you allow to click the button gets verified
     metadata = {
       is_management: 1,
     };
   } catch (e) {
-    e.message = `Error fetching external data: ${e.message}`;
     console.error(e);
-    // If fetching the profile data for the external service fails for any reason,
-    // ensure metadata on the Discord side is nulled out. This prevents cases
-    // where the user revokes an external app permissions, and is left with
-    // stale linked role data.
   }
-
-  // Push the data to Discord.
   await discord.pushMetadata(userId, tokens, metadata);
 }
 
